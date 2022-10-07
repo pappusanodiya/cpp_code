cfeba07ae3f11d93e96434f29585b1caf53870e9
Pappu sanodiya <81160267+pappusanodiya@users.noreply.github.com>
2022-10-07 19:11:33 +0530

    Create first.cpp
    first commit

---
+++ first.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+main(){
+	int x;  //decleration
+	x= 5;  //intialization
+	
+	std::cout << x;  //'::' scope resolution, 'cout' come under iostream header file that's why we have to include iostream header file
+
+}
